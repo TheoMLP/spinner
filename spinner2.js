4cc344307ad52cae4ce366f2862171de164f63eb
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-28 14:06:44 +0000

    fixed

--- spinner2.js
+++ spinner2.js
@@ -2,7 +2,6 @@ const arr = ["|", "/", "—", "\\     \n"];
 let timeInterval= 100;
 
 for (let item of arr) {
-
   setTimeout(() =>  process.stdout.write(`\r${item}`), timeInterval)
   timeInterval += 200;
 }; 
